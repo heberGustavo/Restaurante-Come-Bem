0186e39b8b70da53d7df816aef432faa6c9a1a7c
Heber Gustavo <heber.gbarbosa@hotmail.com>
2021-03-25 12:45:05 -0300

    update

---
+++ RestauranteComeBem/Controllers/MesasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestauranteComeBem.Controllers
+{
+    public class MesasController : Controller
+    {
+        public IActionResult EmAtendimento()
+        {
+            return View();
+        }
+
+        public IActionResult Disponiveis()
+        {
+            return View();
+        }
+
+        public IActionResult Gerenciamento()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestauranteComeBem/Controllers/PerfilController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestauranteComeBem.Controllers
+{
+    public class PerfilController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
