36c1f1ebdc86290e523624b788589605d71b9f60
heberGustavo <heber.gbarbosa@hotmail.com>
2021-05-01 09:47:21 -0300

    .

---
+++ RestauranteComeBem.Domain/Models/EntityDomain/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestauranteComeBem.Domain.Models.EntityDomain
+{
+    public class Usuario
+    {
+        public int id_usuario { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+    }
+}
--- RestauranteComeBem/Startup.cs
+++ RestauranteComeBem/Startup.cs
@@ -1,9 +1,17 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RestauranteComeBem.CrossCutting.DependencyGroups;
+using RestauranteComeBem.CrossCutting.MappingGroups;
+using RestauranteComeBem.Domain.IBusiness.Migration;
+using RestauranteComeBem.Portal.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +21,63 @@ namespace RestauranteComeBem
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
+        public IWebHostEnvironment Env { get; set; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
+                options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+            });
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DomainToData>();
+                cfg.AddProfile<DataToDomain>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            DataDependencyInjection.Register(services);
+            DomainDependencyInjection.Register(services);
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+               .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+               {
+                   options.Cookie.HttpOnly = true;
+                   options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                   options.Cookie.SameSite = SameSiteMode.None;
+                   options.Cookie.Name = ".Restaurante.AuthCookie";
+                   options.LoginPath = "/Home/Login";
+                   options.LogoutPath = "/Logout";
+                   options.Cookie.MaxAge = TimeSpan.FromDays(1);
+               });
+
+            PolicyKeys.ConfigurePolicies(services);
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            services.AddControllersWithViews(options => options.Filters.Add(typeof(ValidateModelState)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, [FromServices] IMigrationBusiness migrationBusiness)
         {
+            migrationBusiness.ExecutarAtualizacaoBancoDados();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,14 +85,11 @@ namespace RestauranteComeBem
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
